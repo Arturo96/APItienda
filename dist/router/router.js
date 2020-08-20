7358991743e999a30c0c018f36353870435174ee
Carlos Vargas <carlosospino13@gmail.com>
2020-08-20 16:58:34 -0500

    CRUD terminado

--- dist/router/router.js
+++ dist/router/router.js
@@ -6,12 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var mysql_1 = __importDefault(require("../mysql/mysql"));
 var router = express_1.Router();
-router.get('/productos', function (req, res) {
+var _ = require("underscore");
+router.get("/productos", function (req, res) {
     var tipoProducto = Number(req.query.tipo) || 0;
     var limite = Number(req.query.limite) || 0;
-    var sqlTipo = tipoProducto <= 0 ? '' : "\n        WHERE tipoProducto = " + mysql_1.default.instance.cnn.escape(tipoProducto) + "\n    ";
-    var sqlLimite = limite <= 0 ? '' : "LIMIT " + limite;
-    var sql = "\n        SELECT id, modelo, marca, descripcion\n        FROM PRODUCTO " + sqlTipo + " " + sqlLimite + "\n    ";
+    var sqlTipo = tipoProducto <= 0
+        ? ""
+        : "\n        WHERE tipoProducto = " + mysql_1.default.instance.cnn.escape(tipoProducto) + "\n    ";
+    var sqlLimite = limite <= 0 ? "" : "LIMIT " + limite;
+    var sql = "\n        SELECT id, modelo, marca, descripcion, fechaPublicacion\n        FROM PRODUCTO " + sqlTipo + " " + sqlLimite + "\n    ";
     mysql_1.default.ejecutarQuery(sql, function (err, productosDB) {
         if (err) {
             return res.status(400).json({
@@ -25,7 +28,7 @@ router.get('/productos', function (req, res) {
         });
     });
 });
-router.get('/productos/:id', function (req, res) {
+router.get("/productos/:id", function (req, res) {
     var id = req.params.id;
     var escapedId = mysql_1.default.instance.cnn.escape(id);
     var sql = "\n        SELECT id, modelo, marca, descripcion\n        FROM PRODUCTO\n        WHERE id=" + escapedId + "\n    ";
@@ -42,24 +45,102 @@ router.get('/productos/:id', function (req, res) {
         });
     });
 });
-router.post('/productos', function (req, res) {
+router.get('/precioAcumulado', function (req, res) {
+    var sql = 'SELECT SUM(precio) AS precioAcumulado FROM PRODUCTO';
+    mysql_1.default.ejecutarQuery(sql, function (err, precioAcumulado) {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err: err
+            });
+        }
+        var suma = precioAcumulado[0].precioAcumulado;
+        res.json({
+            ok: true,
+            suma: suma
+        });
+    });
+});
+router.post("/productos", function (req, res) {
     var body = req.body;
     var producto = {
-        tipo: Number(body.tipo),
-        modelo: body.modelo,
-        marca: body.marca,
-        descripcion: body.descripcion || '',
-        precio: Number(body.precio),
-        cantidad: Number(body.cantidad),
-        meses: Number(body.meses)
+        tipo: mysql_1.default.instance.cnn.escape(Number(body.tipo)),
+        modelo: mysql_1.default.instance.cnn.escape(body.modelo),
+        marca: mysql_1.default.instance.cnn.escape(body.marca),
+        descripcion: mysql_1.default.instance.cnn.escape(body.descripcion) || "",
+        precio: mysql_1.default.instance.cnn.escape(Number(body.precio)),
+        cantidad: mysql_1.default.instance.cnn.escape(Number(body.cantidad)),
+        meses: mysql_1.default.instance.cnn.escape(Number(body.meses))
     };
-    // const escapedId = MySQL.instance.cnn.escape(id);
-    var sql = "\n        INSERT INTO PRODUCTO(tipoProducto, modelo, marca, descripcion,precio, cantidadInv, mesesGarantia)        \n        VALUES(" + producto.tipo + ",\"" + producto.modelo + "\",\"" + producto.marca + "\",\"" + producto.descripcion + "\"," + producto.precio + "," + producto.cantidad + "," + producto.meses + ")\n    ";
+    var sql = "\n        INSERT INTO PRODUCTO(tipoProducto, modelo, marca, descripcion,precio, cantidadInv, mesesGarantia)        \n        VALUES(" + producto.tipo + "," + producto.modelo + "," + producto.marca + "," + producto.descripcion + "," + producto.precio + "," + producto.cantidad + "," + producto.meses + ")\n    ";
+    console.log(sql);
     mysql_1.default.ejecutarQuery(sql, function (err, producto) {
         if (err) {
             return res.status(400).json({
                 ok: false,
                 err: err
+            });
+        }
+        res.json({
+            ok: true,
+            producto: producto
+        });
+    });
+});
+router.put("/productos/:id", function (req, res) {
+    var id = Number(req.params.id) || 0;
+    var escapedId = mysql_1.default.instance.cnn.escape(id);
+    var body = _.pick(req.body, [
+        "tipoProducto",
+        "modelo",
+        "marca",
+        "descripcion",
+        "precio",
+        "cantidadInv"
+    ]);
+    var sql = "UPDATE PRODUCTO SET ";
+    var producto = Object.entries(body);
+    producto.forEach(function (reg, i) {
+        var aux = i == producto.length - 1 ? "" : ",";
+        var attr = reg[0], value = reg[1];
+        sql += attr + " = " + mysql_1.default.instance.cnn.escape(value) + aux + " ";
+    });
+    sql += "WHERE id = " + escapedId;
+    console.log(sql);
+    mysql_1.default.ejecutarQuery(sql, function (err, producto) {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err: err
+            });
+        }
+        if (!producto.affectedRows) {
+            return res.status(400).json({
+                ok: false,
+                err: 'Registro no encontrado'
+            });
+        }
+        res.json({
+            ok: true,
+            producto: producto
+        });
+    });
+});
+router.delete("/productos/:id", function (req, res) {
+    var id = Number(req.params.id) || 0;
+    var escapedId = mysql_1.default.instance.cnn.escape(id);
+    var sql = "DELETE FROM PRODUCTO WHERE id = " + escapedId;
+    mysql_1.default.ejecutarQuery(sql, function (err, producto) {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err: err
+            });
+        }
+        if (!producto.affectedRows) {
+            return res.status(400).json({
+                ok: false,
+                err: 'Registro no encontrado'
             });
         }
         res.json({
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,101 +1,199 @@
-import {Router, Request, Response} from 'express';
-import MySQL from '../mysql/mysql';
+import { Router, Request, Response } from "express";
+import MySQL from "../mysql/mysql";
 
 const router = Router();
+const _ = require("underscore");
 
-router.get('/productos', (req: Request, res: Response) => {
+router.get("/productos", (req: Request, res: Response) => {
+	const tipoProducto = Number(req.query.tipo) || 0;
+	const limite = Number(req.query.limite) || 0;
 
-    const tipoProducto = Number(req.query.tipo) || 0;
-    const limite = Number(req.query.limite) || 0;
-
-    const sqlTipo = tipoProducto <= 0 ? '' : `
+	const sqlTipo =
+		tipoProducto <= 0
+			? ""
+			: `
         WHERE tipoProducto = ${MySQL.instance.cnn.escape(tipoProducto)}
     `;
 
-    const sqlLimite = limite <= 0 ? '' : `LIMIT ${limite}`;
+	const sqlLimite = limite <= 0 ? "" : `LIMIT ${limite}`;
 
-    const sql = `
-        SELECT id, modelo, marca, descripcion
+	const sql = `
+        SELECT id, modelo, marca, descripcion, fechaPublicacion
         FROM PRODUCTO ${sqlTipo} ${sqlLimite}
-    `
+    `;
 
-    MySQL.ejecutarQuery(sql, (err: any, productosDB: Object[]) => {
-        if(err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            })
-        }
+	MySQL.ejecutarQuery(sql, (err: any, productosDB: Object[]) => {
+		if (err) {
+			return res.status(400).json({
+				ok: false,
+				err
+			});
+		}
 
-        res.json({
-            ok: true,
-            productosDB
-        })
-    })
-})
+		res.json({
+			ok: true,
+			productosDB
+		});
+	});
+});
 
-router.get('/productos/:id', (req: Request, res: Response) => {
+router.get("/productos/:id", (req: Request, res: Response) => {
+	const id = req.params.id;
 
-    const id = req.params.id;
+	const escapedId = MySQL.instance.cnn.escape(id);
 
-    const escapedId = MySQL.instance.cnn.escape(id);
-
-    const sql = `
+	const sql = `
         SELECT id, modelo, marca, descripcion
         FROM PRODUCTO
         WHERE id=${escapedId}
-    `
+    `;
 
-    MySQL.ejecutarQuery(sql, (err: any, productosDB: Object[]) => {
+	MySQL.ejecutarQuery(sql, (err: any, productosDB: Object[]) => {
+		if (err) {
+			return res.status(400).json({
+				ok: false,
+				err
+			});
+		}
+
+		res.json({
+			ok: true,
+			producto: productosDB[0]
+		});
+	});
+});
+
+router.get('/precioAcumulado', (req: Request, res: Response) => {
+    const sql = 'SELECT SUM(precio) AS precioAcumulado FROM PRODUCTO';
+
+    MySQL.ejecutarQuery(sql, (err: any, precioAcumulado: any) => {
         if(err) {
-            return res.status(400).json({
+            return res.status(500).json({
                 ok: false,
                 err
             })
         }
 
+        const {precioAcumulado:suma} = precioAcumulado[0] 
+
         res.json({
             ok: true,
-            producto: productosDB[0]
+            suma
         })
     })
 })
 
-router.post('/productos', (req: Request, res: Response) => {
+router.post("/productos", (req: Request, res: Response) => {
+	const { body } = req;
 
-    const {body} = req;
+	let producto = {
+		tipo: MySQL.instance.cnn.escape(Number(body.tipo)),
+		modelo: MySQL.instance.cnn.escape(body.modelo),
+		marca: MySQL.instance.cnn.escape(body.marca),
+		descripcion: MySQL.instance.cnn.escape(body.descripcion) || "",
+		precio: MySQL.instance.cnn.escape(Number(body.precio)),
+		cantidad: MySQL.instance.cnn.escape(Number(body.cantidad)),
+		meses: MySQL.instance.cnn.escape(Number(body.meses))
+	};
 
-    let producto = {
-        tipo: Number(body.tipo),
-        modelo: body.modelo,
-        marca: body.marca,
-        descripcion: body.descripcion || '',
-        precio: Number(body.precio),
-        cantidad: Number(body.cantidad),
-        meses: Number(body.meses)
-    }
-
-    // const escapedId = MySQL.instance.cnn.escape(id);
-
-    const sql = `
+	const sql = `
         INSERT INTO PRODUCTO(tipoProducto, modelo, marca, descripcion,precio, cantidadInv, mesesGarantia)        
-        VALUES(${producto.tipo},"${producto.modelo}","${producto.marca}","${producto.descripcion}",${producto.precio},${producto.cantidad},${producto.meses})
-    `
+        VALUES(${producto.tipo},${producto.modelo},${producto.marca},${producto.descripcion},${producto.precio},${producto.cantidad},${producto.meses})
+    `;
 
-    MySQL.ejecutarQuery(sql, (err: any, producto: any) => {
-        if(err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            })
-        }
+    console.log(sql)
 
-        res.json({
-            ok: true,
-            producto
-        })
-    })
+	MySQL.ejecutarQuery(sql, (err: any, producto: any) => {
+		if (err) {
+			return res.status(400).json({
+				ok: false,
+				err
+			});
+		}
 
-})
+		res.json({
+			ok: true,
+			producto
+		});
+	});
+});
+
+router.put("/productos/:id", (req: Request, res: Response) => {
+	const id = Number(req.params.id) || 0;
+	const escapedId = MySQL.instance.cnn.escape(id);
+
+	const body = _.pick(req.body, [
+		"tipoProducto",
+		"modelo",
+		"marca",
+		"descripcion",
+		"precio",
+		"cantidadInv"
+	]);
+
+	let sql = "UPDATE PRODUCTO SET ";
+
+	const producto = Object.entries(body);
+	producto.forEach((reg, i) => {
+		let aux = i == producto.length - 1 ? "" : ",";
+		const [attr, value] = reg;
+		sql += `${attr} = ${MySQL.instance.cnn.escape(value)}${aux} `;
+	});
+
+	sql += `WHERE id = ${escapedId}`;
+
+	console.log(sql);
+
+	MySQL.ejecutarQuery(sql, (err: any, producto: any) => {
+	    if(err) {
+	        return res.status(500).json({
+	            ok: false,
+	            err
+	        })
+	    }
+
+	    if(!producto.affectedRows) {
+	        return res.status(400).json({
+	            ok: false,
+	            err: 'Registro no encontrado'
+	        })
+	    }
+
+	    res.json({
+	        ok: true,
+	        producto
+	    })
+	})
+
+});
+
+router.delete("/productos/:id", (req: Request, res: Response) => {
+	const id = Number(req.params.id) || 0;
+	const escapedId = MySQL.instance.cnn.escape(id);
+
+	let sql = "DELETE FROM PRODUCTO WHERE id = " + escapedId;
+
+	MySQL.ejecutarQuery(sql, (err: any, producto: any) => {
+	    if(err) {
+	        return res.status(500).json({
+	            ok: false,
+	            err
+	        })
+	    }
+
+	    if(!producto.affectedRows) {
+	        return res.status(400).json({
+	            ok: false,
+	            err: 'Registro no encontrado'
+	        })
+	    }
+
+	    res.json({
+	        ok: true,
+	        producto
+	    })
+	})
+
+});
 
 export default router;

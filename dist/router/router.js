7be500a07e72a3078184895f614c7315c05584c7
Carlos Vargas <carlosospino13@gmail.com>
2020-08-20 11:33:34 -0500

    GET BY ID de Productos Hecho

--- dist/router/router.js
+++ dist/router/router.js
@@ -7,7 +7,11 @@ var express_1 = require("express");
 var mysql_1 = __importDefault(require("../mysql/mysql"));
 var router = express_1.Router();
 router.get('/productos', function (req, res) {
-    var sql = "\n        SELECT id, modelo, marca, descripcion\n        FROM PRODUCTO\n    ";
+    var tipoProducto = Number(req.query.tipo) || 0;
+    var limite = Number(req.query.limite) || 0;
+    var sqlTipo = tipoProducto <= 0 ? '' : "\n        WHERE tipoProducto = " + mysql_1.default.instance.cnn.escape(tipoProducto) + "\n    ";
+    var sqlLimite = limite <= 0 ? '' : "LIMIT " + limite;
+    var sql = "\n        SELECT id, modelo, marca, descripcion\n        FROM PRODUCTO " + sqlTipo + " " + sqlLimite + "\n    ";
     mysql_1.default.ejecutarQuery(sql, function (err, productosDB) {
         if (err) {
             return res.status(400).json({
@@ -18,6 +22,23 @@ router.get('/productos', function (req, res) {
         res.json({
             ok: true,
             productosDB: productosDB
+        });
+    });
+});
+router.get('/productos/:id', function (req, res) {
+    var id = req.params.id;
+    var escapedId = mysql_1.default.instance.cnn.escape(id);
+    var sql = "\n        SELECT id, modelo, marca, descripcion\n        FROM PRODUCTO\n        WHERE id=" + escapedId + "\n    ";
+    mysql_1.default.ejecutarQuery(sql, function (err, productosDB) {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
+        res.json({
+            ok: true,
+            producto: productosDB[0]
         });
     });
 });
--- src/router/router.ts
+++ src/router/router.ts
@@ -5,9 +5,18 @@ const router = Router();
 
 router.get('/productos', (req: Request, res: Response) => {
 
+    const tipoProducto = Number(req.query.tipo) || 0;
+    const limite = Number(req.query.limite) || 0;
+
+    const sqlTipo = tipoProducto <= 0 ? '' : `
+        WHERE tipoProducto = ${MySQL.instance.cnn.escape(tipoProducto)}
+    `;
+
+    const sqlLimite = limite <= 0 ? '' : `LIMIT ${limite}`;
+
     const sql = `
         SELECT id, modelo, marca, descripcion
-        FROM PRODUCTO
+        FROM PRODUCTO ${sqlTipo} ${sqlLimite}
     `
 
     MySQL.ejecutarQuery(sql, (err: any, productosDB: Object[]) => {
@@ -21,6 +30,33 @@ router.get('/productos', (req: Request, res: Response) => {
         res.json({
             ok: true,
             productosDB
+        })
+    })
+})
+
+router.get('/productos/:id', (req: Request, res: Response) => {
+
+    const id = req.params.id;
+
+    const escapedId = MySQL.instance.cnn.escape(id);
+
+    const sql = `
+        SELECT id, modelo, marca, descripcion
+        FROM PRODUCTO
+        WHERE id=${escapedId}
+    `
+
+    MySQL.ejecutarQuery(sql, (err: any, productosDB: Object[]) => {
+        if(err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            producto: productosDB[0]
         })
     })
 })
